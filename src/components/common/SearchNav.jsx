f3b1032654249037092e46f5a46bf996f8bf3794
spyer <sundayomoladee11@gmail.com>
2025-12-26 08:02:34 +0100

    navbar responsiveness

--- src/components/common/SearchNav.jsx
+++ src/components/common/SearchNav.jsx
@@ -5,7 +5,7 @@ const SearchNav = () => {
   return (
     <div className='p-4 bg-green-500 '>
       <section className='container border flex items-center justify-center lg:justify-between'>
-            <div className='flex items-center bg-white rounded-3xl pl-4 pr-4 pt-2 pb-2 w-full border border-amber-800 '> 
+            <div className='flex items-center bg-white rounded-3xl pl-4 pr-4 pt-2 pb-2 '> 
             <select name="All Categories" id="" className=' text-sm'>
                <option value="All Categories">All Categories</option>
                 <option value="Electronics">Electronics</option>
