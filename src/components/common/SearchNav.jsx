3cd71464e9dce630d018aa4b65cf3ac01ffc7f74
spyer <sundayomoladee11@gmail.com>
2025-12-26 08:18:43 +0100

    navbar responsiveness

--- src/components/common/SearchNav.jsx
+++ src/components/common/SearchNav.jsx
@@ -7,14 +7,14 @@ const SearchNav = () => {
       <section className='container border flex items-center justify-center lg:justify-between'>
             <div className='flex items-center bg-white rounded-3xl pl-4 pr-4 pt-2 pb-2 w-full'> 
             <select name="All Categories" id="" className=' text-sm'>
-               <option value="All">All </option>
+               <option value="All_categories">All Categories</option>
                 <option value="Electronics">Electronics</option>
                 <option value="Phones">Phones</option>
                 <option value="PC">PC</option>
                 <option value="Clothes">Clothes</option>
                 <option value="Shoes">Shoes</option>
             </select>
-            <input type="search" name="" className='p-1 text-sm' placeholder='Search Anything' id="" />
+            <input type="search" name="" className='p-1 text-sm w-20' placeholder='Search Anything' id="" />
             <Search size={18} className='text-black '/>
         </div>
         <div className='hidden lg:block'><p className='text-white '>FREE SHIPPING OVER $199</p></div>
